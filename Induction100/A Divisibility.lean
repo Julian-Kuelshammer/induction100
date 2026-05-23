5a444324969d285bc882b2468b41532d77427f5e
Julian Kuelshammer <julian.kuelshammer@math.uu.se>
2026-05-23 08:54:40 +0200

    add 8th induction

--- Induction100/A Divisibility.lean
+++ Induction100/A Divisibility.lean
@@ -8,11 +8,10 @@ induction n with
 | zero => norm_num -- For n=0, this is just a calculation.
 | succ n ih =>
   obtain ⟨m,hm⟩ := ih -- Choose m such that n ^ 2 + n = 2 * m
-  use m + n + 1 -- We claim that then (n + 1) ^ 2 + (n + 1) = 2 * (m + n + 1).
+  use n + 1 + m -- We claim that then (n + 1) ^ 2 + (n + 1) = 2 * (m + n + 1).
   -- Rewrite to isolate (n ^ 2 + n) and use the induction hypothesis.
-  rw [add_assoc m]
-  nth_rewrite 5 [add_comm]
-  rw [add_assoc, ← add_assoc m, ← hm]
+  nth_rewrite 7 [add_comm]
+  rw [← add_assoc m, add_assoc, ← add_assoc m (m + n), ← add_assoc m, ← hm]
   ring -- The rest is using the ring axioms.
 
 /- 2) n ^ 3 + 2 * n is divisible by 3. -/
@@ -21,9 +20,9 @@ induction n with
 | zero => norm_num
 | succ n ih =>
   obtain ⟨m,hm⟩ := ih
-  use m + n ^ 2 + n + 1
+  use n ^ 2 + n + 1 + m
   -- Rewrite to isolate (3 * m) and use the induction hypothesis.
-  rw [mul_add 3, mul_add 3, mul_add 3, ← hm]
+  rw [mul_add 3, ← hm]
   ring
 
 /- 3) 4 * n ^ 3 - n is divisible by 3. -/
@@ -33,9 +32,9 @@ induction n with
 | zero => norm_num
 | succ n ih =>
   obtain ⟨m,hm⟩ := ih
-  use m + 4 * n ^ 2 + 4 * n + 1
+  use 4 * n ^ 2 + 4 * n + 1 + m
   -- Rewrite to isolate (3 * m) and use the induction hypothesis.
-  rw [add_assoc m, mul_add 3, mul_add 3, ← hm]
+  rw [mul_add 3, ← hm]
   push_cast
   ring
 
@@ -52,7 +51,7 @@ induction n with
     norm_cast
     rw [hl]
     ring
-  use m + l
+  use l + m
   rw [mul_add, ← hm, h']
   push_cast
   ring
@@ -63,8 +62,8 @@ induction n with
 | zero => norm_num
 | succ n ih =>
   obtain ⟨m,hm⟩ := ih
-  use m + n ^ 2 + 2 * n + 1
-  rw [add_assoc m, mul_add 6, mul_add 6, ← hm]
+  use n ^ 2 + 2 * n + 1 + m
+  rw [mul_add 6, ← hm]
   ring
 
 /- 6) n ^ 3 - 6 * n ^ 2 + 14 * n is divisible by 3. -/
@@ -73,8 +72,8 @@ induction n with
 | zero => norm_num
 | succ n ih =>
   obtain ⟨m,hm⟩ := ih
-  use m + n ^ 2 - 3 * n + 3
-  rw [mul_add, mul_sub, mul_add, ← hm]
+  use n ^ 2 - 3 * n + 3 + m
+  rw [mul_add, ← hm]
   push_cast
   ring
 
@@ -89,4 +88,14 @@ induction n with
     rw [← hm]
     ring
   rw [pow_succ, this]
+  ring
+
+/- 8) n ^ 3 + (n + 1) ^ 3 + (n + 2) ^ 3 is divisible by 9. -/
+example : 9 ∣ n ^ 3 + (n + 1) ^ 3 + (n + 2) ^ 3 := by
+induction n with
+| zero => norm_num
+| succ n ih =>
+  obtain ⟨m,hm⟩ := ih
+  use n ^ 2 + 3 * n + 3 + m
+  rw [mul_add, ← hm]
   ring
